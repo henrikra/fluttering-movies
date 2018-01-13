d8d41dc9fc2fe06a4b8ef4a6644dc1371f9a2a90
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-01-14 00:14:04 +0200

    Make scrolling look cooler

--- lib/single_movie.dart
+++ lib/single_movie.dart
@@ -41,131 +41,136 @@ class SingleMovieState extends State<SingleMovie> {
     }
     return new Scaffold(
       appBar: new AppBar(title: new Text(_movie['title'])),
-      body: new ListView(
+      body: new Stack(
         children: <Widget>[
-          new Stack(
+          new Container(color: const Color.fromRGBO(14, 16, 27, 1.0),),
+          new Image.network(
+              "https://image.tmdb.org/t/p/w500${_movie['backdrop_path']}", height: 211.0,),
+          new Positioned(
+            child: new Container(
+              decoration:
+                  new BoxDecoration(color: const Color.fromRGBO(0, 0, 0, 0.5)),
+            ),
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            height: 211.0,
+          ),
+          new ListView(
             children: <Widget>[
-              new Image.network(
-                  "https://image.tmdb.org/t/p/w500${_movie['backdrop_path']}"),
-              new Positioned(
-                child: new Container(
-                  decoration:
-                      new BoxDecoration(color: const Color.fromRGBO(0, 0, 0, 0.5)),
-                ),
-                bottom: 0.0,
-                top: 0.0,
-                left: 0.0,
-                right: 0.0,
-              ),
-              new Container(
-                child: new Column(
-                  children: <Widget>[
-                    new Container(
-                      child: new Text(
-                        _movie['title'],
-                        style: new TextStyle(
-                          color: Colors.white,
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "Roboto",
-                          letterSpacing: 1.0,
-                          height: 1.25,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      margin: const EdgeInsets.only(top: 50.0),
-                    ),
-                    new Container(
-                      margin: const EdgeInsets.only(top: 20.0),
-                      child: new Text("${_movie['runtime'].toString()} min", style: new TextStyle(
-                        color: new Color.fromRGBO(255, 255, 255, 0.7)
-                      )),
-                    ),
-                    new Container(
-                      child: new Text(
-                        _movie['overview'],
-                        style: new TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0,
-                            fontFamily: "Roboto",
-                            height: 1.25),
-                      ),
-                      margin: new EdgeInsets.symmetric(vertical: 20.0),
-                    ),
-                    new Row(
+              new Stack(
+                children: <Widget>[
+                  new Container(
+                    child: new Column(
                       children: <Widget>[
-                        new Expanded(
-                          flex: 2,
-                          child: new Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              new Text('DIRECTOR', style: new TextStyle(
-                                color: new Color.fromRGBO(255, 255, 255, 0.7)
-                              )),
-                              new Container(
-                                child: new Text(_director, style: new TextStyle(color: Colors.white),),
-                                margin: const EdgeInsets.only(top: 5.0),
-                              )
-                            ],
+                        new Container(
+                          child: new Text(
+                            _movie['title'],
+                            style: new TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Roboto",
+                              letterSpacing: 1.0,
+                              height: 1.25,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
+                          margin: const EdgeInsets.only(top: 50.0),
                         ),
-                        new Expanded(
-                          flex: 3,
-                          child: new Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              new Text('GENRES', style: new TextStyle(
-                                color: new Color.fromRGBO(255, 255, 255, 0.7)
-                              )),
-                              new Container(
-                                child: new Text(
-                                  _genres, 
-                                  style: new TextStyle(color: Colors.white),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                margin: const EdgeInsets.only(top: 5.0),
-                              )
-                            ],
+                        new Container(
+                          margin: const EdgeInsets.only(top: 20.0),
+                          child: new Text("${_movie['runtime'].toString()} min", style: new TextStyle(
+                            color: new Color.fromRGBO(255, 255, 255, 0.7)
+                          )),
+                        ),
+                        new Container(
+                          child: new Text(
+                            _movie['overview'],
+                            style: new TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontFamily: "Roboto",
+                                height: 1.25),
                           ),
+                          margin: new EdgeInsets.symmetric(vertical: 20.0),
+                        ),
+                        new Row(
+                          children: <Widget>[
+                            new Expanded(
+                              flex: 2,
+                              child: new Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  new Text('DIRECTOR', style: new TextStyle(
+                                    color: new Color.fromRGBO(255, 255, 255, 0.7)
+                                  )),
+                                  new Container(
+                                    child: new Text(_director, style: new TextStyle(color: Colors.white),),
+                                    margin: const EdgeInsets.only(top: 5.0),
+                                  )
+                                ],
+                              ),
+                            ),
+                            new Expanded(
+                              flex: 3,
+                              child: new Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  new Text('GENRES', style: new TextStyle(
+                                    color: new Color.fromRGBO(255, 255, 255, 0.7)
+                                  )),
+                                  new Container(
+                                    child: new Text(
+                                      _genres, 
+                                      style: new TextStyle(color: Colors.white),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    margin: const EdgeInsets.only(top: 5.0),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-                padding: new EdgeInsets.all(20.0),
-                margin: const EdgeInsets.only(top: 211.0),
-                decoration: new BoxDecoration(
-                    gradient: new LinearGradient(
-                  begin: const Alignment(-0.5, -0.5),
-                  end: const Alignment(0.0, 0.0),
-                  colors: <Color>[
-                    const Color.fromRGBO(75, 35, 42, 1.0),
-                    const Color.fromRGBO(14, 16, 27, 1.0),
-                  ],
-                )),
-              ),
-              new Positioned(
-                top: 100.0,
-                left: 0.0,
-                right: 0.0,
-                child: new Align(
-                  alignment: Alignment.center,
-                  child: new Container(
-                    decoration: new BoxDecoration(boxShadow: <BoxShadow>[
-                      new BoxShadow(
-                        color: new Color.fromRGBO(0, 0, 0, 0.2),
-                        offset: new Offset(10.0, 10.0),
-                      )
-                    ]),
-                    child: new Image.network(
-                      "https://image.tmdb.org/t/p/w300${_movie['poster_path']}",
-                      height: 150.0,
+                    ),
+                    padding: new EdgeInsets.all(20.0),
+                    margin: const EdgeInsets.only(top: 211.0),
+                    decoration: new BoxDecoration(
+                        gradient: new LinearGradient(
+                      begin: const Alignment(-0.25, -0.5),
+                      end: const Alignment(0.0, 0.0),
+                      colors: <Color>[
+                        const Color.fromRGBO(75, 35, 42, 1.0),
+                        const Color.fromRGBO(14, 16, 27, 1.0),
+                      ],
+                    )),
+                  ),
+                  new Positioned(
+                    top: 100.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: new Align(
+                      alignment: Alignment.center,
+                      child: new Container(
+                        decoration: new BoxDecoration(boxShadow: <BoxShadow>[
+                          new BoxShadow(
+                            color: new Color.fromRGBO(0, 0, 0, 0.2),
+                            offset: new Offset(10.0, 10.0),
+                          )
+                        ]),
+                        child: new Image.network(
+                          "https://image.tmdb.org/t/p/w300${_movie['poster_path']}",
+                          height: 150.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
+                ],
+              )   
             ],
-          )   
+          )
         ],
       ),
     );
