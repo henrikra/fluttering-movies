91f31d10fa4555dfedaa4f5723bb4b44bfa7e908
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-01-13 12:40:44 +0200

    Fix indent

--- lib/single_movie.dart
+++ lib/single_movie.dart
@@ -28,31 +28,30 @@ class SingleMovie extends StatelessWidget {
             ],
           ),
           new Container(
-              child: new Center(
-                child: new Column(
-                  children: <Widget>[
-                    new Image.network("https://image.tmdb.org/t/p/w300${movie['poster_path']}", height: 150.0,),
-                    new Container(
-                      child: new Text(movie['title'], style: new TextStyle(
-                        color: Colors.white, fontSize: 22.0,
-                      ), textAlign: TextAlign.center,),
-                      margin: const EdgeInsets.only(top: 20.0),
-                    )
-                  ],
-                ),
-              ),
-              decoration: new BoxDecoration(
-                gradient: new LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: const Alignment(0.0, 0.0),
-                  colors: <Color>[
-                    const Color.fromRGBO(75, 35, 42, 1.0),
-                    const Color.fromRGBO(14, 16, 27, 1.0),
-                  ],
-                )
+            child: new Center(
+              child: new Column(
+                children: <Widget>[
+                  new Image.network("https://image.tmdb.org/t/p/w300${movie['poster_path']}", height: 150.0,),
+                  new Container(
+                    child: new Text(movie['title'], style: new TextStyle(
+                      color: Colors.white, fontSize: 22.0,
+                    ), textAlign: TextAlign.center,),
+                    margin: const EdgeInsets.only(top: 20.0),
+                  )
+                ],
               ),
             ),
-          
+            decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                begin: Alignment.topLeft,
+                end: const Alignment(0.0, 0.0),
+                colors: <Color>[
+                  const Color.fromRGBO(75, 35, 42, 1.0),
+                  const Color.fromRGBO(14, 16, 27, 1.0),
+                ],
+              )
+            ),
+          ),
         ],
       ) ,
     );
